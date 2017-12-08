936133148483c036815f2057ef662b34b2131e79
Wolper <diksondelgado@yahoo.com.br>
2017-12-08 13:48:52 -0200

    atualizando
    atualizando

--- admin/_models/AdminEtapaVoo.class.php
+++ admin/_models/AdminEtapaVoo.class.php
@@ -4,7 +4,7 @@
  * AdminEmpresa.class [ MODEL ADMIN ]
  * Responável por gerenciar as empresas no admin do sistema!
  * 
- * @copyright (c) 2014, Robson V. Leite UPINSIDE TECNOLOGIA
+ * @copyright (c) 2017, Dikson Delgado
  */
 class AdminEtapaVoo {
 
