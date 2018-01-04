82fb951952e39aad7b006431764f82dcf6f2afeb
Wolper_1 <Wolper_1@Wolper_1-PC>
2018-01-04 15:42:23 -0200

    terminado instalação...ainda com bugs melhorias na visulização da tabela principal

--- admin/system/home.php
+++ admin/system/home.php
@@ -2,50 +2,57 @@
 <div class="content home">
 
     <h1>Manutenções</h1>
-    <table border="0" width="100%" style="text-align: left">
-        <tr style="background: black; color: white;">
-            <th>Descrição</th>
-            <th>PN</th>
-            <th>SN</th>
-            <th>TL</th>
-            <th>TC</th>
-            <th>In-Anv</th>
-            <th>In-Data</th>
-            <th>In-TSN</th>
-            <th>In-TSO</th>
-            <th>Venc. Tempo</th>
-            <th>Venc. Data</th>
-            <th>Disp. Tempo</th>
-            <th>Venc. Data</th>
-        </tr>
+    <div class="table-responsive">
+        <table class="table table-striped table-bordered table-hover">
+            <thead class="text-uppercase text-center" style="background: black; color: white;">
+                <tr>
+                    <th>Descrição</th>
+                    <th>PN</th>
+                    <th>SN</th>
+                    <th>TL</th>
+                    <th>TC</th>
+                    <th>In/Anv</th>
+                    <th>In/Data</th>
+                    <th>In/TSN</th>
+                    <th>In/TSO</th>
+                    <th>Venc/Tempo</th>
+                    <th>Venc/Data</th>
+                    <th>Disp/Tempo</th>
+                    <th>Venc/Data</th>
+                </tr>
+            </thead>
+            <tbody class="text-uppercase text-center bg-success">
+                <?php
+                $readInsp = new Read();
+                $readInsp->FullRead('SELECT * FROM inspecao AS i JOIN tipo_inspecao AS ti ON i.idInspecao = ti.id_tipo_inspecao');
 
-<?php
-$readInsp = new Read();
-$readInsp->FullRead('SELECT * FROM inspecao AS i JOIN tipo_inspecao AS ti ON i.idInspecao = ti.id_tipo_inspecao');
+                if (!$readInsp->getRowCount() > 0):
+                    echo 'Ainda não há dados das aeronaves cadastrados';
+                else:
+                    foreach ($readInsp->getResult() as $query):
+                        extract($query);
 
-if (!$readInsp->getRowCount() > 0):
-    echo 'Ainda não há dados das aeronaves cadastrados';
-else:
-    extract($readInsp->getResult()[0]);
+                        echo '<tr>';
+                        echo '<td>' . (str_replace('-', ' ', $descricaoInspecao)) . '</td>';
+                        echo '<td>' . $pnInspecao . '</td>';
+                        echo '<td>' . $snInspecao . '</td>';
+                        echo '<td>' . $tlInspecao . '</td>';
+                        echo '<td>' . $tcInspecao . '</td>';
+                        echo '<td>' . $in_anvInspecao . '</td>';
+                        echo '<td>' . $in_dataInspecao . '</td>';
+                        echo '<td>' . $in_tsnInspecao . '</td>';
+                        echo '<td>' . $in_tsoInspecao . '</td>';
+                        echo '<td>' . $vencimento_for_time . '</td>';
+                        echo '<td>' . $vencimento_for_date . '</td>';
+                        echo '<td>' . $disponivel_for_time . '</td>';
+                        echo '<td>' . $disponivel_for_date . '</td>';
+                        echo '</tr>';
+                    endforeach;
 
-    echo '<td>' . $descricaoInspecao . '</td>';
-    echo '<td>' . $pnInspecao . '</td>';
-    echo '<td>' . $snInspecao . '</td>';
-    echo '<td>' . $tlInspecao . '</td>';
-    echo '<td>' . $tcInspecao . '</td>';
-    echo '<td>' . $in_anvInspecao . '</td>';
-    echo '<td>' . $in_dataInspecao . '</td>';
-    echo '<td>' . $in_tsnInspecao . '</td>';
-    echo '<td>' . $in_tsoInspecao . '</td>';
-    echo '<td>' . $vencimento_for_time . '</td>';
-    echo '<td>' . $vencimento_for_date . '</td>';
-    echo '<td>' . $disponivel_for_time . '</td>';
-    echo '<td>' . $disponivel_for_date . '</td>';
-    echo '<tr>';
-
-endif;
-?>
-
-    </table>
+                endif;
+                ?>
+            </tbody>
+        </table>
+    </div>
     <div class="clear"></div>
 </div> <!-- content home -->
